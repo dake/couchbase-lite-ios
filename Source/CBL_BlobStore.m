1f2473a63a0dcf66a0a03d8e883789a45af418d9
Pasin Suriyentrakorn <pasin@couchbase.com>
2015-04-13 16:17:16 -0700

    Remove NSDataReadingMappedIfSafe from CBL_BlobStore.blobForKey
    #649

--- Source/CBL_BlobStore.m
+++ Source/CBL_BlobStore.m
@@ -120,8 +120,7 @@
 
 - (NSData*) blobForKey: (CBLBlobKey)key {
     NSString* path = [self rawPathForKey: key];
-    NSData* blob =  [NSData dataWithContentsOfFile: path options: NSDataReadingMappedIfSafe
-                                             error: NULL];
+    NSData* blob =  [NSData dataWithContentsOfFile: path options: 0 error: NULL];
     if (_encryptionKey && blob) {
         blob = [_encryptionKey decryptData: blob];
         CBLBlobKey decodedKey = [[self class] keyForBlob: blob];
